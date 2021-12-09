c57fa3738befd2b4e63f932d40a4e060f30fdd50
druvamurali <druvamurali2812@gmail.com>
2021-12-09 15:07:57 -0600

    Add eventlistener for clicks on vrblog page heading.

--- js/main.js
+++ js/main.js
@@ -33,3 +33,21 @@ function resumeHeadingClick(event) {
   heading.innerText = 'Resume: Druva Murali';
   heading.className = 'is-clicked';
 }
+
+
+// VRblog
+
+var h1 = document.querySelector('#vrblog h1');
+
+document.querySelector('html').className = 'js';
+
+if (h1) {
+  h1.addEventListener('click', vrblogHeadingClick);
+}
+
+function vrblogHeadingClick(event) {
+  console.log('Title was clicked to display the blog of Valentino Rossi');
+  var heading = event.target;
+  heading.innerText = 'Blog: Valentino Rossi';
+  heading.className = 'is-clicked';
+}
